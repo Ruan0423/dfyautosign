165ad71e0454f33e69a3b8ddc507f17d06066281
Ruan0423 <0802traveler@gmail.com>
2026-01-03 15:29:06 +0800

    更改端口

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
 	router := backend.SetupRouter()
 
 	// 启动服务器
-	log.Println("服务器启动在 http://localhost:8080")
-	if err := router.Run(":8080"); err != nil {
+	log.Println("服务器启动在 http://localhost:10087")
+	if err := router.Run(":10087"); err != nil {
 		log.Fatalf("服务器启动失败: %v", err)
 	}
 }
